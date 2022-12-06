ae485852c67db8eb5a43392b89f36eaf969c9f96
Raphael Stedler <r.stedler@gmx.de>
2022-12-06 15:58:32 +0100

    First try

---
+++ frontend/src/app/articles/articles.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArticleService } from '../services/article.service';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit {
+
+  constructor(private articleService: ArticleService, private router: Router) { }
+
+  articles: any;
+
+  ngOnInit(): void {
+    this.showArticles();
+  }
+
+  showArticles(){
+    this.articles = this.articleService.listArticles().subscribe(article =>{
+      this.articles = article;
+      console.log(this.articles);
+    });
+  }
+
+  deleteArticle(id:any) {
+    this.articleService.deleteArticle(id).subscribe(
+      res => {
+        this.articles = this.articles.filter((a:any) => a.id == id);
+      }
+    );
+    this.router.navigateByUrl('/');
+  }
+
+}
